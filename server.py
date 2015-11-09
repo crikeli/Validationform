7c2fd30bfb3c963d93d4d59aa443f7378e4887f8
Kelin Christi <christi.kelin20@gmail.com>
2015-11-09 13:14:46 -0800

    added the result.html file and modified it

--- server.py
+++ server.py
@@ -6,25 +6,24 @@ app.secret_key = "keysareeverywhere"
 def index():
 	return render_template("index.html")
 
-
-@app.route('/result', methods=['POST'])
-def result():
-
+@app.route('/info', methods=['POST'])
+def info():
 	session['name'] = request.form['name']
 	session['location'] = request.form['location']
 	session['language'] = request.form['language']
 	session['comment'] = request.form["comment"]
-
 	if len(request.form['name']) < 1:
 		flash("The name field cannot be left blank!")
-	else: flash("Success!")
-
-	if len(request.form['comment']) < 1:
+	elif len(request.form['comment']) < 1:
 		flash("The comment field cannot be left blank!")
 	elif len(request.form)['comment'] > 120:
 		flash("The comment cannot be greater than 120 characters!")
 	else:
 		flash("Success!")
 	return redirect('/')
+
+@app.route('/result')
+def result():
+	return render_template("result.html")
 
 app.run(debug=True)
