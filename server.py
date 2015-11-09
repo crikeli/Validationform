547545ba70bf5f29fca1e6b9339994b712d4eb8e
Kelin Christi <christi.kelin20@gmail.com>
2015-11-09 11:34:31 -0800

    Deleted JS file and added flash to display alert messages

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, session                                          
+from flask import Flask, render_template, request, redirect, session, flash                                          
 app = Flask(__name__)                     
-app.secret_key = "keys"
+app.secret_key = "keysareeverywhere"
 
 @app.route('/')
 def index():
@@ -10,9 +10,19 @@ def index():
 @app.route('/result', methods=['POST'])
 def formdata():
 	session['name'] = request.form['name']
+	if len(request.form ['name']) < 1:
+		flash("The name field cannot be left blank!")
+	else: 
+		flash("Success!")
 	session['location'] = request.form['location']
 	session['language'] = request.form['language']
 	session['comment'] = request.form["comment"]
+	if len(request.form ['comment']) < 1:
+		flash("The comment field cannot be left blank!")
+	elif len(request.form) ['comment'] > 120:
+		flash("The comment cannot be greater than 120 characters!")
+	else:
+		flash("Success!")
 
 	return redirect('/showdata')
 
--- static/js/myscript.js
+++
@@ -1,1 +0,0 @@
-alert('please fill in the info')
